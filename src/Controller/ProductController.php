e747a11056d819c92343020364dab9454df42dcd
kenzo-b <contact@kbriche.fr>
2024-11-22 08:55:04 +0100

    search method add

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,25 +5,35 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends AbstractController
 {
-#[Route('/product/show-all', name: 'product_show_all')]
-public function showAll(ProductRepository $productRepository): Response
-{
-    $products = $productRepository->findAll();
-    return $this->render('product/product_show_all.html.twig', ['products' => $products]);
-}
+    #[Route('/product/show-all', name: 'product_show_all')]
+    public function showAll(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+        return $this->render('product/product_show_all.html.twig', ['products' => $products]);
+    }
 
-#[Route('/product/show/{id}', name: 'product_show', requirements: ['id' => '\d+'])]
-public function show(int $id, ProductRepository $productRepository): Response {$product = $productRepository->find($id);
-
-        if(!$product) {
+    #[Route('/product/show/{id}', name: 'product_show', requirements: ['id' => '\d+'])]
+    public function show(int $id, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->find($id);
+        if (!$product) {
             throw new NotFoundHttpException("ce produit n'existe pas");
         }
         return $this->render('product/product_show.html.twig', ['product' => $product]);
+    }
+
+    #[Route('/product/search', name: 'product_search', methods: ['POST'])]
+    public function search(Request $request, ProductRepository $productRepository): Response
+    {
+        $KeywordSearched = $request->request->get('searchProduct');
+        $products =  $productRepository->search($KeywordSearched);
+        return $this->render('product/product_show_all.html.twig', ['products' => $products]);
     }
 }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -4,12 +4,47 @@ namespace App\Repository;
 
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 class ProductRepository extends ServiceEntityRepository
 {
+    private QueryBuilder $qb;
+
+    private string $alias = 'pdt';
+
+    private function initializeQueryBuilder(): void {
+        $this->qb = $this->createQueryBuilder($this->alias)
+            ->select($this->alias);
+    }
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+
+    private function orNameLike(string $keyword): void {
+        $this->qb
+            ->orWhere("$this->alias.name LIKE :name")
+            ->setParameter('name', "%$keyword%");
+    }
+
+    private function orDescriptionLike(string $keyword): void {
+        $this->qb
+            ->orWhere("$this->alias.description LIKE :name")
+            ->setParameter('name', "%$keyword%");
+    }
+
+    private function orPropertyLike(string $propertyName,string $keyword): void {
+        $this->qb
+            ->orWhere("$this->alias.$propertyName LIKE :$propertyName")
+            ->setParameter($propertyName, "%$keyword%");
+    }
+
+    public function search(string $keyword): array {
+        $this->initializeQueryBuilder();
+        $this->orPropertyLike('description', $keyword);
+        $this->orPropertyLike('name', $keyword);
+        return $this->qb->getQuery()->getResult();
     }
 }
