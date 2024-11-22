62ea8fc8b9bad917b24220d0f8aff207fcad409c
kenzo-b <contact@kbriche.fr>
2024-11-22 10:41:35 +0100

    add form target and submit button

--- src/Controller/ManageProductController.php
+++ src/Controller/ManageProductController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Form\Type\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,7 +15,14 @@ class ManageProductController extends AbstractController
     #[Route('/manage/product/new', name: 'manage_product_new')]
     public function new(): Response {
         $product = new Product();
-        $form = $this->createForm(ProductType::class, $product);
+        $form = $this->createForm(
+            ProductType::class,
+            $product,
+            ['action' => $this->generateUrl('manage_product_new')]
+        );
+
+        $form->add('submit', SubmitType::class);
+
         return $this->renderForm('product/product_new.html.twig', ['form' => $form]);
     }
 }
